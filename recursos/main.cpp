dab40d95f6b3d78e3beb87396833d3ebf3a405e2
Emmanuelito18 <emmanuelito18yt@gmail.com>
2023-12-20 14:50:57 -0600

    definicion de funcion gotoxy

--- recursos/main.cpp
+++ recursos/main.cpp
@@ -13,7 +13,10 @@ int main(int argc, char** argv) {
 }
 
 void gotoxy(int x,int y){
-	
+	COORD coord;
+	coord.X=x;
+	coord.Y=y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
 
 void setColor(int color){
