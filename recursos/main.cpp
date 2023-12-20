29b8a8a66a1f392ab2aae0054a13b2ae56e915df
Emmanuelito18 <emmanuelito18yt@gmail.com>
2023-12-20 13:36:19 -0600

    primer commit

---
+++ recursos/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout<<"Hola mundo"<<endl;
+	return 0;
+}
