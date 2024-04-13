1d525aade9aff07441f1adabe969f2b572169768
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-04-12 19:25:18 -0600

    Se añadió el codigo fuente de todos los métodos de la clase

---
+++ recursos/Portada.h
@@ -0,0 +1,20 @@
+#ifndef PORTADA_H
+#define PORTADA_H
+
+class Portada
+{
+	public:
+		//Constructor
+		Portada();
+		//Destructor
+		~Portada();
+		void mostrarPortada();
+	protected:
+		void irA(int,int);
+		void ocultarCursor();
+		void mostrarCursor();
+		void portada();
+		void animacion();
+};
+
+#endif
